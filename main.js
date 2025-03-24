b47f24c11e4927e14338ecd4770f76718050b6e3
Guilherme <134606761+lsrrw@users.noreply.github.com>
2025-03-24 12:26:34 -0300

    xx

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let = trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
 trilho.AddEventListener('click', ()=>{
 trilho.classList.toggle('dark')
+body.classList.toggle('dark')
 })
